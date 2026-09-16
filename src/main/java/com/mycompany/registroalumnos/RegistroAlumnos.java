da37d6b67188b5bf612198830fd1d7112731a6c7
Patrick Garcia <fabriziogavidia8@gmail.com>
2026-09-15 23:04:33 -0500

    otro nombre

--- src/main/java/com/mycompany/registroalumnos/RegistroAlumnos.java
+++ src/main/java/com/mycompany/registroalumnos/RegistroAlumnos.java
@@ -12,7 +12,7 @@ public class RegistroAlumnos {
 
    public static void main(String[] args) {
 
-        String nombre = "Fabrizio";
+        String nombre = "especial aea ";
         String curso = "Programación";
         double notas = 20;
 
