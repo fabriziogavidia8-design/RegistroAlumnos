9910961d5abe4eca6cd9dbfddea65b485d46e2a0
Patrick Garcia <fabriziogavidia8@gmail.com>
2026-09-15 20:56:01 -0500

    Primer commit del proyecto RegistroAlumnos

---
+++ src/main/java/com/mycompany/registroalumnos/RegistroAlumnos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registroalumnos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class RegistroAlumnos {
+
+   public static void main(String[] args) {
+
+        String nombre = "Patrick";
+        String curso = "Programación";
+
+        System.out.println("===== REGISTRO DE ALUMNOS =====");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Curso: " + curso);
+        System.out.println("Alumno registrado correctamente.");
+    }
+}
