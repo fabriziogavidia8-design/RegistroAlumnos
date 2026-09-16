284f2ae059ec902ea510c337bbe7d9af5888244e
Patrick Garcia <fabriziogavidia8@gmail.com>
2026-09-15 22:31:49 -0500

    cambio de nombre

--- src/main/java/com/mycompany/registroalumnos/RegistroAlumnos.java
+++ src/main/java/com/mycompany/registroalumnos/RegistroAlumnos.java
@@ -12,7 +12,7 @@ public class RegistroAlumnos {
 
    public static void main(String[] args) {
 
-        String nombre = "Patrick";
+        String nombre = "Fabrizio";
         String curso = "Programación";
         double notas = 20;
 
